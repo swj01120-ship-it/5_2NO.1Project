33641e068fdda2553368e07c9fa50fc6c038549c
swj01120-ship-it <swj01120@gmail.com>
2025-10-29 21:47:31 +0900

    노트트레일 효과

--- Assets/Scripts/JudgeLineEffect.cs
+++ Assets/Scripts/JudgeLineEffect.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class JudgeLineEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Material lineMaterial;
+    public float pulseSpeed = 2f;
+    public float minIntensity = 1f;
+    public float maxIntensity = 3f;
+
+    private float currentIntensity;
+
     void Start()
     {
-        
+        if (lineMaterial == null)
+        {
+            Renderer renderer = GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                lineMaterial = renderer.material;
+            }
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (lineMaterial != null)
+        {
+            // Ping-Pong으로 왔다갔다하는 효과
+            currentIntensity = Mathf.Lerp(minIntensity, maxIntensity,
+                Mathf.PingPong(Time.time * pulseSpeed, 1f));
+
+            // Emission 강도 조절
+            Color emissionColor = Color.red * currentIntensity;
+            lineMaterial.SetColor("_EmissionColor", emissionColor);
+        }
     }
 }
