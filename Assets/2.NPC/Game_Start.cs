76c00050fd83a5074420a0150a1117e37a1b93a8
swj01120-ship-it <swj01120@gmail.com>
2025-11-03 12:18:36 +0900

    오프닝 연결, 북 UV 1개

---
+++ Assets/2.NPC/Game_Start.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;      // 타임라인 제어
+using UnityEngine.SceneManagement; // 씬 전환(선택)
+
+public class StartOnClick : MonoBehaviour
+{
+    [SerializeField] PlayableDirector director;  // 인트로 타임라인
+    [SerializeField] Animator characterAnimator; // 캐릭터 애니메이터(선택)
+    [SerializeField] string startTrigger = "StartGame";
+    [SerializeField] string nextScene = "MainMenuScene";
+    [SerializeField] bool loadNextScene = true;
+
+    void OnMouseDown()  // 오브젝트를 클릭하면 자동 호출(콜라이더 필요)
+    {
+        if (director && director.state == PlayState.Playing) director.Stop();
+        if (characterAnimator && !string.IsNullOrEmpty(startTrigger))
+            characterAnimator.SetTrigger(startTrigger);
+
+        if (loadNextScene)
+            SceneManager.LoadScene(nextScene);
+    }
+}
